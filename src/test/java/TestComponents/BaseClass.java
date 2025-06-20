30e41193e8fd6d0c80260cae6a5940baa46afc67
YeshwanthMyc <yeshwanthdhanaraj310@gmail.com>
2025-06-20 15:56:47 +0530

    Fixed Minor issues

--- src/test/java/TestComponents/BaseClass.java
+++ src/test/java/TestComponents/BaseClass.java
@@ -180,7 +180,7 @@ public class BaseClass {
 	public static String Deduction;
 	public static void invData() {
 		 invDocNumber=null;
-		 Deduction=System.getProperty("Deduction", "Penalty");
+		 Deduction=System.getProperty("Deduction", "None");
 	}
 
 	public BaseClass() {
